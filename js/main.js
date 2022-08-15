e7dd7a948affd325ac8e6486a1240e3b9cfefaf4
german montenegro <germontenegro715@gmail.com>
2022-08-15 17:24:07 -0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,61 +1,69 @@
-let nombres=document.querySelectorAll(".nombres");
+let nombres = document.querySelectorAll(".nombres");
 
-nombres.className="red";
 
 
 class Producto {
-    constructor(precio, nombre,stock,vencimiento) {
-      this.precio = precio;
-      this.nombre = nombre;
-      this.stock= stock;
-      this.vencimiento=vencimiento;
-    }
-     
-    mostrarProducto(){
-        let mensaje =`<li>${this.nombre}</li>`
-        document.querySelector(".Producto").innerHTML +=mensaje;
-       
+    constructor( nombre,precio, stock, vencimiento) {
+        this.nombre = nombre;
+        this.precio = parseFloat(precio);
+        this.stock = stock;
+        this.vencimiento = vencimiento;
     }
 
-    mostrarPrecio(){
-        let mensaje =`<li>$${this.precio}</li>`
-        document.querySelector(".Precio").innerHTML +=mensaje;
+    mostrarProducto() {
+        let mensaje = `<li>${this.nombre}</li>`
+        document.querySelector(".Producto").innerHTML += mensaje;
+
     }
 
-    mostrarStock(){
-        let mensaje =`<li>${this.stock}</li>`
-        document.querySelector(".Stock").innerHTML +=mensaje;
+    mostrarPrecio() {
+        let mensaje = `<li>$${this.precio}</li>`
+        document.querySelector(".Precio").innerHTML += mensaje;
     }
-    mostrarVencimiento(){
-        let mensaje =`<li>${this.vencimiento}</li>`
-        document.querySelector(".Vencimiento").innerHTML +=mensaje;
+
+    mostrarStock() {
+        let mensaje = `<li>${this.stock}</li>`
+        document.querySelector(".Stock").innerHTML += mensaje;
     }
-    
-  }
-  
-  const productos = [];
-  let nombre = "";
-  let precio = "";
-  let stock = "";
-  let vencimiento = "";
-  
-//   para cargar productos
-  function ingresarNuevoProducto() {
-    let confirmar=confirm("desea cargar datos");
-    while (confirmar){
-   
-    nombre = prompt("ingrese el nombre");
-    precio = prompt("ingrese el precio");
-    stock = prompt("ingrese el stock");
-    vencimiento= prompt("ingrese el vencimiento");
-
-    const nuevoProducto = new Producto(precio, nombre,stock, vencimiento);
-    productos.push(nuevoProducto);
-    
-
-    confirmar=confirm("desea cargar datos");
+    mostrarVencimiento() {
+        let mensaje = `<li>${this.vencimiento}</li>`
+        document.querySelector(".Vencimiento").innerHTML += mensaje;
+    }
 
 }
+
+const productos = [];
+let nombre = "";
+let precio = "";
+let stock = "";
+let vencimiento = "";
+
+//   para cargar productos
+function ingresarNuevoProducto() {
+    let confirmar = confirm("desea cargar datos");
+    while (confirmar) {
+
+        nombre = prompt("ingrese el nombre");
+        precio = parseFloat(prompt("ingrese el precio"));
+        while (isNaN(precio)) {
+            alert("usar dato numerico");
+            precio = parseFloat(prompt("ingrese el precio"));
+        }
+
+        stock = prompt("ingrese el stock");
+        while (isNaN(stock)) {
+            alert("usar dato numerico");
+            stock= parseFloat(prompt("ingrese el precio"));
+        }
+        vencimiento= prompt("ingrese el vencimiento");
+
+        const nuevoProducto = new Producto(nombre,precio, stock, vencimiento);
+        productos.push(nuevoProducto);
+       
+
+        confirmar = confirm("desea cargar datos");
+
+    }
 
     for (Producto of productos) {
 
@@ -66,25 +74,25 @@ class Producto {
 
     }
 
-  }
+}
 // busacar los productos
 
-  function buscarProducto(){
-    
-let buscarPorNombre=prompt("buscar nombre");
-buscarPorNombre =productos.find((el) => el.nombre === buscarPorNombre);
-console.log(buscarPorNombre);
-  }
+function buscarProducto() {
+
+    let buscarPorNombre = prompt("buscar nombre");
+    buscarPorNombre = productos.find((el) => el.nombre === buscarPorNombre);
+    console.log(buscarPorNombre);
+}
 
 //   buscar el producto con mator stock
 
-  function buscarStock(){
-   
-   let buscarStock=+prompt("Escribir cantidad de stock");
-   buscarStock=buscarStock=productos.filter((el)=>el.stock > buscarStock);
-   console.log(buscarStock);
+function buscarStock() {
 
-  }
+    let buscarStock = +prompt("Escribir cantidad de stock");
+    buscarStock = buscarStock = productos.filter((el) => el.stock > buscarStock);
+    console.log(buscarStock);
+
+}
 
 //   imprimir todo los productos
 
@@ -92,4 +100,3 @@ function  ImprimirTodo(){
     let imprimirTodo=productos.slice();
     console.log(imprimirTodo);
 }
-
